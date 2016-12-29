2d5413661f03425f3500eaa7951422ea19e4d9ef
DWoodley <woodley.dean@yahoo.com>
2016-12-29 00:17:26 -0500

    First Commit Here

--- ReproducibleResearchProject1.R
+++ ReproducibleResearchProject1.R
@@ -1,14 +1,6 @@
----
-title: "PA1_template.Rmd"
-author: "Dean Woodley"
-date: "Monday, December 26, 2016"
-output: html_document
----
-Loading and preprocessing the data
-Show any code that is needed to Load the data
+## Reproducible Research Graded Project 1
 
-1: Code for reading in the dataset and/or processing the data
-```{r echo=FALSE}
+## 1. Read in Dataset and process data.
     #File name activity.csv
     if(exists("activity")) remove("activity")
     if(exists("imputed.activity")) remove("imputed.activity")
@@ -16,12 +8,9 @@ Show any code that is needed to Load the data
     if(exists("avg.steps.by.time")) remove("avg.steps.by.time")
     if(exists("avg.steps.weekday.t")) remove("avg.steps.weekday.t")
     if(exists("avg.steps.weekend.t")) remove("avg.steps.weekend.t")
-
+    
     par(mfrow=c(1,1))
-```
-
-```{r echo=TRUE}
-
+    
     library(lubridate)
     library(stringr)
     library(data.table)
@@ -41,31 +30,27 @@ Show any code that is needed to Load the data
     activity$datetime <- as_datetime(activity$datetime,tz=Sys.timezone())
 
     # Create factors from intervals
-    activity$interval.f <- factor(str_c(str_sub(str_pad(as.character(activity$interval),4,
-                                side="left",pad="0"),start=1,end=2),
-                                 str_sub(str_pad(as.character(activity$interval),4,
-                                        side="left",pad="0"),start=3,end=4),sep=":"),ordered=TRUE)
-```
-2: Create a histogram of number of steps per day.
-```{r, echo=TRUE}
+    activity$interval.f <- factor(str_c(str_sub(str_pad(as.character(activity$interval),4,side="left",pad="0"),start=1,end=2),
+                                 str_sub(str_pad(as.character(activity$interval),4,side="left",pad="0"),start=3,end=4),
+                                 sep=":"),ordered=TRUE)
+
+## 2. Create histogram of number of steps per day.
     hist(tapply(activity$steps,activity$date,sum,na.rm=TRUE),breaks=50,
          xlab = "Number of steps",
          ylab = "Frequency",
          main = "Frequency of Total Steps Per Day \nOver 61 Day Period (Raw Data)")
-```
 
-3:  Mean and median number of steps taken each day.
-```{r, echo=TRUE}
+## 3. Calculate mean and median of number of steps per day.
     mean.steps <- round(mean(tapply(activity$steps,activity$date,sum,na.rm=TRUE)))
     median.steps <- median(tapply(activity$steps,activity$date,sum,na.rm=TRUE))
     
     print(str_c("Mean number of steps per day:",as.character(mean.steps),sep= " "))
     print(str_c("Median number of steps per day:",as.character(median.steps),sep= " "))
+    
 
-```
-
-4:  Time series plot of the average number of steps taken
-```{r, echo=TRUE}
+## 4. Create time series plot of the average number of steps taken.
+    # This is a time series plot of the average number of steps taken
+    # by 5 minute intervals.
     avg.steps.by.time <- tapply(activity$steps,activity$interval,mean,na.rm=TRUE,ordered=TRUE)
     
     t.avg.steps <- as.data.table(avg.steps.by.time)
@@ -82,24 +67,27 @@ Show any code that is needed to Load the data
          xlab = "Time of Day",
          type = "l")
     lines(t.avg.steps$avg.steps.by.time,col="blue")    
-```
-   
-   
-5:  The 5-minute interval that, on average, contains the maximum number of steps
-```{r}
+    
+## 5. Find the 5 minute interval that, on average, contains the maximum
+##    number of steps.
     max.interval <- t.avg.steps[t.avg.steps$avg.steps.by.time 
                                 == max(t.avg.steps$avg.steps.by.time)]$interval
 
-    print(str_c("Start of 5 minute interval with maximum average number of steps:",
-                as.character(max.interval),sep= " "))
-```
+    print(str_c("Start of 5 minute interval with maximum average number of steps:",as.character(max.interval),sep= " "))
 
-
-6:  Code to describe and show a strategy for imputing missing data
-```{r}
     
+## 7. Calculate and report the total number of missing values in the dataset 
+    # (i.e. the total number of rows with NAs)
     num.missing.vals <- sum(is.na(activity$steps))
+    print(str_c("Count of missing steps values:",as.character(num.missing.vals),sep= " "))
+    
+    
+## 6. Create code to describe and show a strategy for imputing missing data.
+    # Will imput missing data where the value of the steps column is NA.
+    # Will assume that 0 values of the steps are legitimate data points.
+    
     imputed.activity <- activity
+    
     if (num.missing.vals > 0)
     {
         for (x in t.avg.steps$interval) {
@@ -108,9 +96,9 @@ Show any code that is needed to Load the data
         }
     }
 
-```
-7:  Histogram of the total number of steps taken each day after missing values are imputed
-```{r, echo=TRUE}
+
+## 7. Create histogram of the total number of steps taken each day AFTER 
+##    missing values are imputed. (Compare to histogram of raw data)
     xpar <- par(mfrow = c(2,1))
     
     hist(tapply(imputed.activity$steps,imputed.activity$date,sum),breaks=50,
@@ -124,9 +112,19 @@ Show any code that is needed to Load the data
          main = "Frequency of Total Steps Per Day \nOver 61 Day Period (Raw Data)")
     
     par(xpar)
-```
-8:  Panel plot comparing the average number of steps taken per 5-minute interval across weekdays and weekends
-```{r, echo=TRUE}
+    
+    mean.steps.imputed <- round(mean(tapply(imputed.activity$steps,imputed.activity$date,sum,na.rm=TRUE)))
+    median.steps.imputed <- median(tapply(imputed.activity$steps,imputed.activity$date,sum,na.rm=TRUE))
+    
+    print(str_c("Mean number of steps per day (raw data):",as.character(mean.steps),sep= " "))
+    print(str_c("Mean number of steps per day (imputed data):",as.character(mean.steps.imputed),sep= " "))
+    print(str_c("Median number of steps per day (raw data):",as.character(median.steps),sep= " "))
+    print(str_c("Median number of steps per day (imputed data):",as.character(median.steps.imputed),sep= " "))
+    
+## 8. Create a panel plot comparing the average number of steps taken per
+##    per 5-minute interval across weekdays and weekends.
+    
+    
     imputed.activity$daytype <- factor("Weekday",levels=c("Weekday","Weekend"))
     imputed.activity[is.weekend(imputed.activity$datetime),]$daytype <- factor("Weekend")
     
@@ -142,7 +140,7 @@ Show any code that is needed to Load the data
     
     avg.steps.weekday.t <- as.data.table(avg.steps.weekday)
     avg.steps.weekday.t$interval <- row.names(avg.steps.weekday)
- 
+    
     xpar <- par(mfrow = c(2,1))
     plot(x = factor(avg.steps.weekday.t$interval,ordered=TRUE),
          y = avg.steps.weekday.t$avg.steps.weekday,
@@ -161,7 +159,15 @@ Show any code that is needed to Load the data
     lines(avg.steps.weekend.t$avg.steps.weekend,col="blue") 
 
     par(xpar)
-```
-
-
-
+    
+#    t.avg.steps <- as.data.table(avg.steps.by.time)
+#    t.avg.steps$avg.steps.by.time <- round(t.avg.steps$avg.steps.by.time)
+#    t.avg.steps$interval <- row.names(avg.steps.by.time)
+#    t.avg.steps$interval <- str_pad(t.avg.steps$interval,4,side="left",pad="0")
+#    t.avg.steps$interval <- str_c(str_sub(t.avg.steps$interval,1,2),":",str_sub(t.avg.steps$interval,3,4))
+#    t.avg.steps$interval <- factor(t.avg.steps$interval,ordered = TRUE)
+    
+    
+    
+## 9. Include all the R code needed to reproduce the results (numbers, plots,
+##    etc) in the report.
